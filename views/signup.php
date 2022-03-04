38381470f9625fdaacf6d71b97e313c0da50998d
VictorG222 <72720374+VictorG222@users.noreply.github.com>
2022-03-04 16:41:35 -0700

    better gym signup

--- views/signup.php
+++ views/signup.php
@@ -1,23 +1,13 @@
 <!DOCTYPE html>
 <html>
 <style>
-body {font-family: Arial, Helvetica, sans-serif;}
+body {font-family: Arial, Helvetica, sans-serifs}
 * {box-sizing: border-box}
 
 /* Full-width input fields */
-input[type=text], input[type=password] {
-  width: 100%;
-  padding: 15px;
-  margin: 5px 0 22px 0;
-  display: inline-block;
-  border: none;
-  background: #f1f1f1;
-}
 
-input[type=text]:focus, input[type=password]:focus {
-  background-color: #ddd;
-  outline: none;
-}
+
+
 
 hr {
   border: 1px solid #f1f1f1;
@@ -26,7 +16,7 @@ hr {
 
 /* Set a style for all buttons */
 button {
-  background-color: #04AA6D;
+  background-color: #77B64D;
   color: white;
   padding: 14px 20px;
   margin: 8px 0;
@@ -71,28 +61,33 @@ button:hover {
   }
 }
 </style>
-<body>
+<body >
 
-<form action="/action_page.php" style="border:1px solid #ccc">
+<form action="/action_page.php" style="border:1px solid #ccc; background-color: #F5F5F5">
   <div class="container">
-    <h1>Sign Up</h1>
-    <p>Please fill in this form to create an account.</p>
+
+    <img src="..\res\img\bettergym.png" alt="bettergym" width="500" height="600">
+
+
+    <h3>Registro de usuario</h3>
+    <p>Porfavor llene el formulario de registro.</p>
     <hr>
 
-    <label for="email"><b>Email</b></label>
-    <input type="text" placeholder="Enter Email" name="email" required>
+    <label for="nombre"><b>Nombre</b></label>
+    <input type="text" placeholder="Ingrese su nombre" name="nombre" required>
 
-    <label for="psw"><b>Password</b></label>
-    <input type="password" placeholder="Enter Password" name="psw" required>
+    <label for="apellido"><b>Apellido</b></label>
+    <input type="text" placeholder="Ingrese su apellido" name="apellido" required>
 
-    <label for="psw-repeat"><b>Repeat Password</b></label>
-    <input type="password" placeholder="Repeat Password" name="psw-repeat" required>
-    
-    <label>
-      <input type="checkbox" checked="checked" name="remember" style="margin-bottom:15px"> Remember me
-    </label>
-    
-    <p>By creating an account you agree to our <a href="#" style="color:dodgerblue">Terms & Privacy</a>.</p>
+    <label for="edad"><b>Edad</b></label>
+    <input type="text" placeholder="Ingrese su edad" name="edad" required>
+
+    <label for="sexo"><b>Sexo</b></label>
+    <input type="text" placeholder="Ingrese su sexo" name="sexo" required>
+
+    <label for="fecha"><b>Fecha de registro</b></label>
+    <input type="date"  name="fecha" required>
+
 
     <div class="clearfix">
       <button type="button" class="cancelbtn">Cancel</button>
